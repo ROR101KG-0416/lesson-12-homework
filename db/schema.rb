775338b7ca0f7feb90ae8b3a0bc321d3b925e946
David Noriega <dnori817@gmail.com>
2016-05-25 22:40:14 -0400

    Completed Milestone #1

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160526003142) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "postal_code"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
